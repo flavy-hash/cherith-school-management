dc1f1cffcb3276e7883defe60140d620b40fc41c
flavy-hash <flavianmichael663@gmail.com>
2026-04-22 16:45:15 +0300

    Add header action to navigate to Class Combined Report
    - Add 'View Class Combined Report' header action button
    - Provide direct navigation from Combined Results list to class report
    - Use document-report icon for visual clarity

--- app/Filament/Resources/CombinedResultResource/Pages/ListCombinedResults.php
+++ app/Filament/Resources/CombinedResultResource/Pages/ListCombinedResults.php
@@ -3,9 +3,20 @@
 namespace App\Filament\Resources\CombinedResultResource\Pages;
 
 use App\Filament\Resources\CombinedResultResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCombinedResults extends ListRecords
 {
     protected static string $resource = CombinedResultResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('class_report')
+                ->label('Class Report')
+                ->icon('heroicon-o-table-cells')
+                ->url(CombinedResultResource::getUrl('class-report')),
+        ];
+    }
 }
